e008e6b3b884c9f03c529a05488df35c0a8a204b
Francisco Gutierrez <franciscogutierrez@Franciscos-MBP.home>
2021-02-25 18:01:11 -0500

    MaxNOfDaysaYear

---
+++ src/MaxNOfDaysaYear.java
@@ -0,0 +1,6 @@
+public class MaxNOfDaysaYear {
+    public static void main(String[] args) {
+        short MaxNofDaysaYear= 366;
+        System.out.println("Max n. of days in a Year is 366 days" );
+    }
+}
