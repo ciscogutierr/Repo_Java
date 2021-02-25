09bf9cfa0a5d8b089cf8c24dbd29fd45bf90083e
Francisco Gutierrez <franciscogutierrez@Franciscos-MBP.home>
2021-02-25 18:20:25 -0500

    TotalNumberofEmployeesinOrganization

---
+++ src/TotalNumberofEmployeesinOrganization.java
@@ -0,0 +1,7 @@
+public class TotalNumberofEmployeesinOrganization {
+    public static void main(String[] args) {
+        int TotalNumberOfEmployees = 100000;
+        System.out.println("Total of employees in an organization are " + TotalNumberOfEmployees);
+
+    }
+}
