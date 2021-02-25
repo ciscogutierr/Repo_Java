009bb0e9c7058b24f8201180cced83c553443b22
Francisco Gutierrez <franciscogutierrez@Franciscos-MBP.home>
2021-02-25 17:35:46 -0500

    Numberofhoursinaday

---
+++ src/Numberofhoursinaday.java
@@ -0,0 +1,6 @@
+public class Numberofhoursinaday {
+    public static void main(String[] args) {
+        byte NumOfHoursInDay = 24;
+        System.out.println("There are 24 hours in a day");
+    }
+}
--- src/first/project/firstCiscoJavaClass.java
+++ src/first/project/firstCiscoJavaClass.java
@@ -2,6 +2,7 @@ package first.project;
 
 public class firstCiscoJavaClass {
     public static void main(String[] args) {
+
         System.out.println("Hello Cisco!!");
     }
 
