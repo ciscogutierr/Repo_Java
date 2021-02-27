4adbb1d5403b5d699cb213691255d99dbef3620b
Francisco Gutierrez <franciscogutierrez@Franciscos-MBP.home>
2021-02-27 18:43:57 -0500

    Homework

---
+++ src/first/project/Homework2.java
@@ -0,0 +1,43 @@
+package first.project;
+
+public class Homework2 {
+    //Number of Hours in a Day
+    //public static void main(String[] args) {
+    //    byte numOfHrsInADay = 24;
+      //  System.out.println("Number of Hours in a Day " + numOfHrsInADay);
+   // }
+//max no. of days in a year
+//public static void main(String[] args) {
+ //   short maxNumOfDaysInAYear = 366;
+   // System.out.println("Max number of days in a year " +maxNumOfDaysInAYear);
+//}
+
+//Total Number of Employees in Organization
+//public static void main(String[] args) {
+//    int totalNumOfEmployeesInOrg = 100000;
+//    System.out.println("Total num of Employees in a organization are  " +totalNumOfEmployeesInOrg);
+//}
+//public static void main(String[] args) {
+//   Long  PopulationInACountry= 50000000L;
+//   System.out.println("PopulationIn a Country is " + PopulationInACountry);
+
+ //   public static void main(String[] args) {
+ //       float interestRate = 2.5f;
+ //       System.out.println("Interest Rate is " +interestRate);
+
+//    public static void main(String[] args) {
+//        double BalinBankAct= 500.75;
+//        System.out.println("Balance in Bank Acount is " +BalinBankAct);
+
+//        public static void main(String[] args) {
+//            boolean DoesTheSunRiseOnTheWest = false;
+//            System.out.println("Does the Sun Rise on the West? " + DoesTheSunRiseOnTheWest);
+
+//    public static void main(String[] args) {
+//        char FirstNameInitial= 'F';
+//        char LastNameInitial= 'G';
+//        System.out.println("The initials of my last name are: " +FirstNameInitial+LastNameInitial);
+//    public static void main(String[] args) {
+//        double BalinBankAct= 500.75;System.out.println("Balance in Bank Acount is " +BalinBankAct);
+    }
+}
